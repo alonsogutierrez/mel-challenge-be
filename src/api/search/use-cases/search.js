7fc0af510d97307e58cb6ce56eb5ed74300f7800
vn0jbpg <alonso.gutierrez@walmart.com>
2021-06-03 18:22:25 -0400

    feat(search): add logic to get all categories from search and ubication

--- src/api/search/use-cases/search.js
+++ src/api/search/use-cases/search.js
@@ -13,18 +13,47 @@ const getProductsByText = async text => {
   );
   if (isValidResponse(response)) {
     logger.info('Valid response');
-    const { results, paging, available_filters } = response;
+    const { results, paging, available_filters, filters } = response;
     delete response.results;
     logger.info('Response: ', JSON.stringify(response));
     if (areProductsInResponse(paging)) {
       logger.info('Existen productos');
       const productsResult = results;
-      const categoriesResult = available_filters[0].values;
+      let categoriesResult = [];
+      let categoryFilter = filters.find(filter => filter.id === 'category');
+      if (!categoryFilter) {
+        const categoriesAvailables = available_filters.find(
+          filter => filter.id === 'category'
+        );
+        categoriesResult = categoriesAvailables.values
+          .map(category => category.name)
+          .sort((a, b) => a.results > b.results)
+          .slice(0, 4);
+      } else {
+        categoryFilter.values.forEach(value => {
+          value.path_from_root.forEach(filter =>
+            categoriesResult.push(filter.name)
+          );
+        });
+      }
+
+      const ubications = available_filters.find(
+        filter => filter.id === 'state'
+      );
+      const ubicationAvailable = ubications.values.sort(
+        (a, b) => a.results > b.results
+      )[0];
+
       const author = {
         name: 'Alonso',
         lastname: 'Gutierrez'
       };
-      const response = createResponse(categoriesResult, productsResult, author);
+      const response = createResponse(
+        categoriesResult,
+        productsResult,
+        author,
+        ubicationAvailable
+      );
       return response;
     }
     logger.info('No hay productos');
@@ -44,25 +73,17 @@ const areProductsInResponse = paging => {
   return total > 0 ? true : false;
 };
 
-const createResponse = (categories, products, author) => {
+const createResponse = (categories, products, author, ubication) => {
   const response = {
     author: {
       name: author.name,
       lastname: author.lastname
     },
-    categories: getProductCategories(categories),
-    items: getProductsInfo(products)
+    categories: categories,
+    items: getProductsInfo(products),
+    ubication: ubication.name
   };
   return response;
-};
-
-const getProductCategories = categories => {
-  let productCategories = [];
-  categories.forEach(category => {
-    const { name } = category;
-    productCategories.push(name);
-  });
-  return productCategories;
 };
 
 const getProductsInfo = products => {
